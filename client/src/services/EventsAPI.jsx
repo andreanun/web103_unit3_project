393e36d4747ae25ed3fee6a3e472d53a1466b888
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2026-06-21 21:04:14 -0400

    added frontedn api functions

---
+++ client/src/services/EventsAPI.jsx
@@ -0,0 +1,13 @@
+const getAllEvents = async () => {
+    const response = await fetch('/api/events')
+    const data = await response.json()
+    return data
+}
+
+const getEventsById = async (id) => {
+    const response = await fetch(`/api/events/${id}`)
+    const data = await response.json()
+    return data
+}
+
+export default { getAllEvents, getEventsById }
