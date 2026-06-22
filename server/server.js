03058f9b3b6fcd906382a82fb0e5be51abf7d7da
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2026-06-21 20:53:24 -0400

    updated server.js with routes

--- server/server.js
+++ server/server.js
@@ -1,36 +1,32 @@
-import express from 'express'
-import path from 'path'
-import favicon from 'serve-favicon'
-import dotenv from 'dotenv'
+import express from "express";
+import path from "path";
+import favicon from "serve-favicon";
+import dotenv from "dotenv";
+import events from "./routes/events.js";
+import locations from "./routes/locations.js";
 
-// import the router from your routes file
+dotenv.config();
 
+const PORT = process.env.PORT || 3000;
 
-dotenv.config()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
 
-const app = express()
-
-app.use(express.json())
-
-if (process.env.NODE_ENV === 'development') {
-    app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')))
-}
-else if (process.env.NODE_ENV === 'production') {
-    app.use(favicon(path.resolve('public', 'party.png')))
-    app.use(express.static('public'))
+if (process.env.NODE_ENV === "development") {
+  app.use(favicon(path.resolve("../", "client", "public", "party.png")));
+} else if (process.env.NODE_ENV === "production") {
+  app.use(favicon(path.resolve("public", "party.png")));
+  app.use(express.static("public"));
 }
 
-// specify the api path for the server to use
+app.use('/api/locations', locations)
+app.use('/api/events', events)
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (_, res) =>
-        res.sendFile(path.resolve('public', 'index.html'))
-    )
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (_, res) => res.sendFile(path.resolve("public", "index.html")));
 }
 
 app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`)
-})
+  console.log(`server listening on http://localhost:${PORT}`);
+});
